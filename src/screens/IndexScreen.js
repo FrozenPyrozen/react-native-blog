3b611c46ac1e8d3e3d491ecba7793d00de0ea99b
FrozenPyrozen <kiril.kohan@gmail.com>
2019-09-12 18:03:27 -0400

    feat ( context )

---
+++ src/screens/IndexScreen.js
@@ -0,0 +1,24 @@
+import React, { useContext } from 'react';
+import { StyleSheet, Button, Text, View, FlatList } from 'react-native';
+import BlogContext from '../context/BlogContext';
+
+const IndexScreen = () => {
+  const { data, addBlogPost } = useContext(BlogContext);
+
+  return (
+    <View>
+      <Text>IndexScreen</Text>
+      <Button title="Add post" onPress={addBlogPost} />
+
+      <FlatList
+        data={data}
+        keyExtractor={blogPost => blogPost.title}
+        renderItem={({ item }) => <Text>{item.title}</Text>}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default IndexScreen;
